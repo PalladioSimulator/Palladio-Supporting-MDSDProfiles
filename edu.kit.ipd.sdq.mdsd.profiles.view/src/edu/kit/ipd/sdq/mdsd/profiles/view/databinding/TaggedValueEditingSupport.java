29cd82400bba4c1900a150624ef2810d9b723d9d
taspolat <taspolat@fzi.de>
2014-04-03 08:30:07 +0000

    * PROFILES-10 : logic for the Boolean editing added
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/Profiles/trunk@21601 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.mdsd.profiles.view/src/edu/kit/ipd/sdq/mdsd/profiles/view/databinding/TaggedValueEditingSupport.java
+++ edu.kit.ipd.sdq.mdsd.profiles.view/src/edu/kit/ipd/sdq/mdsd/profiles/view/databinding/TaggedValueEditingSupport.java
@@ -1,6 +1,6 @@
 package edu.kit.ipd.sdq.mdsd.profiles.view.databinding;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +8,6 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.util.Enumerator;
-import org.eclipse.emf.databinding.IEMFObservable;
 import org.eclipse.emf.databinding.edit.IEMFEditObservable;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EDataType;
@@ -16,31 +15,18 @@ import org.eclipse.emf.ecore.EEnum;
 import org.eclipse.emf.ecore.EEnumLiteral;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.ETypedElement;
-import org.eclipse.emf.ecore.util.EDataTypeUniqueEList;
-import org.eclipse.emf.edit.command.ChangeCommand;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ComboBoxCellEditor;
 import org.eclipse.jface.viewers.ComboBoxViewerCellEditor;
 import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.TableItem;
-import org.modelversioning.emfprofile.EMFProfilePackage;
-import org.modelversioning.emfprofile.Stereotype;
-import org.modelversioning.emfprofileapplication.StereotypeApplication;
-
-
-
-
 //import edu.kit.ipd.sdq.mdsd.profiles.view.viewer.ChangedListener;
-import edu.kit.ipd.sdq.mdsd.profiles.view.viewer.ProfilePropertiesView;
 
 public class TaggedValueEditingSupport extends EditingSupport {
 
@@ -84,12 +70,10 @@ public class TaggedValueEditingSupport extends EditingSupport {
     @Override
     protected CellEditor getCellEditor(Object element) {
         EAttribute attr = (EAttribute) ((IEMFEditObservable) element).getStructuralFeature();
-
-        if (((Object) attr.getEType()) instanceof String) {
-
-            return textEditor;
-
-        } else if (((Object) attr.getEType()) instanceof EEnum) {
+        Object obj = ((EObject) ((IEMFEditObservable) element).getObserved()).eGet(attr);
+        Object dataType = (Object) attr.getEType();
+        
+        if (dataType instanceof EEnum) {
             logger.info(((Object) attr.getEType()));
 
             EEnum eEnum = (EEnum) ((EAttribute) attr).getEType();
@@ -110,26 +94,22 @@ public class TaggedValueEditingSupport extends EditingSupport {
             } while (j <= 0);
             return comboBoxEditor;
 
-            // return new ComboBoxCellEditor(viewer.getTable(), elements,
-            // SWT.DROP_DOWN | SWT.READ_ONLY);
-        } else if (((Object) attr.getEType()) instanceof EDataType) {
-            logger.info(((Object) attr.getEType()));
-            Boolean eDataType = (Boolean) ((EAttribute) attr).getDefaultValue();
-//            Boolean eBoolean = Boolean.getBoolean(((EDataType) attr.getEType()).;
-//            literals = eBoolean;
-//            String[] elements = new String[literals.size()];
-//            int i = 0;
-//            for (EEnumLiteral literal : literals) {
-//                elements[i++] = literal.getName();
-//            }
-            logger.info("asdfsadfasdfasdf " + eDataType);
-            comboBoxEditor = new ComboBoxViewerCellEditor(viewer.getTable(), SWT.DROP_DOWN | SWT.READ_ONLY);
-            comboBoxEditor.setLabelProvider(new LabelProvider());
-            comboBoxEditor.setContenProvider(new ArrayContentProvider());      
-//            comboBoxEditor.setInput(new Boolean[]{eDataType.booleanValue(), Boolean.valueOf(true)});
+        } else if (dataType instanceof EDataType) {
+        	EDataType eDataType = (EDataType) dataType;
+        	
+        	if (eDataType.getInstanceClass() == String.class || eDataType.getInstanceClass() == int.class) {
+        		return textEditor;
+        	
+        	} else if (eDataType.getInstanceClass() == Boolean.class || eDataType.getInstanceClass() == Boolean.TYPE) {
+        		comboBoxEditor = new ComboBoxViewerCellEditor(viewer.getTable(), SWT.DROP_DOWN | SWT.READ_ONLY);
+        		comboBoxEditor.setLabelProvider(new LabelProvider());
+        		comboBoxEditor.setContenProvider(new ArrayContentProvider());      
+        		comboBoxEditor.setInput(Arrays.asList(new Boolean[] {(Boolean) obj, !(Boolean) obj}));
+        		
+        	}
             return comboBoxEditor;
+            
         } else {
-
             return new TextCellEditor(viewer.getTable());
 
         }
@@ -179,8 +159,18 @@ public class TaggedValueEditingSupport extends EditingSupport {
                 }
 
             } else if (obj instanceof Boolean) {
-                // TODO: Implement Boolean combo box
-
+            	Boolean oldBoolean = Boolean.valueOf((Boolean) obj);
+            	Boolean newBoolean = Boolean.valueOf((Boolean) value);
+        			if (newBoolean == null || (newBoolean.toString()).trim().length() == 0) {
+                        cmd = SetCommand.create(editingDomain, ((IEMFEditObservable) element).getObserved(), attr, 
+                        		null);
+                        editingDomain.getCommandStack().execute(cmd);
+        			} else if (oldBoolean != null && oldBoolean != newBoolean) {
+                        cmd = SetCommand.create(editingDomain, ((IEMFEditObservable) element).getObserved(), attr, 
+                        		newBoolean);
+                        editingDomain.getCommandStack().execute(cmd);
+        			}
+        			
             } else {
                 // do nothing
             }
