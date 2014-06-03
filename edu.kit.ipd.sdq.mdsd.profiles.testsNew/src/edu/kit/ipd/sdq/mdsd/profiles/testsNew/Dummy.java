ecaab5af903008018d3b1692ec26e55c2d3600f2
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-06-03 08:56:43 +0000

    Added buckminster to mdsd profiles
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/Profiles/trunk@22779 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ edu.kit.ipd.sdq.mdsd.profiles.testsNew/src/edu/kit/ipd/sdq/mdsd/profiles/testsNew/Dummy.java
@@ -0,0 +1,23 @@
+package edu.kit.ipd.sdq.mdsd.profiles.testsNew;
+
+import org.junit.Test;
+
+public class Dummy {
+
+    @Test
+    public void test() {
+        /**
+         * edu.kit.ipd.sdq.mdsd.profiles.tests has a bad project layout: checking it out should
+         * directly result in an Eclipse project, not in a folder containing projects! [Lehrig]
+         * 
+         * Note: This plugin is triggered by our build server
+         */
+
+        // TODO Extract JUnit tests from edu.kit.ipd.sdq.mdsd.profiles.tests to this plug-in
+        // TODO Extract other tests from edu.kit.ipd.sdq.mdsd.profiles.tests to somewhere ;)
+        // TODO Get rid of the original edu.kit.ipd.sdq.mdsd.profiles.tests and rename this project
+        // to edu.kit.ipd.sdq.mdsd.profiles.tests.
+        // TODO Remove all comments here
+    }
+
+}
