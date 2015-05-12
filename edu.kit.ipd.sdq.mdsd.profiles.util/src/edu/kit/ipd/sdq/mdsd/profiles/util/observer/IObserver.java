4364e619d927280d92204cbdd285724437602cdc
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-05-12 17:17:35 +0000

    Added FIXME
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/Profiles/trunk@26512 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.mdsd.profiles.util/src/edu/kit/ipd/sdq/mdsd/profiles/util/observer/IObserver.java
+++ edu.kit.ipd.sdq.mdsd.profiles.util/src/edu/kit/ipd/sdq/mdsd/profiles/util/observer/IObserver.java
@@ -4,14 +4,16 @@
 package edu.kit.ipd.sdq.mdsd.profiles.util.observer;
 
 /**
+ * FIXME Why do you not use the observer pattern from palladio commons? see:
+ * org.palladiosimulator.commons.designpatterns.AbstractObservable [Lehrig]
+ * 
  * @author Matthias Eisenmann
  * @param <T>
  */
 public interface IObserver<T> {
 
     /**
-     * Update method to be called when this observer shall be notified for the
-     * given observable
+     * Update method to be called when this observer shall be notified for the given observable
      * 
      * @param observable
      *            the observable
