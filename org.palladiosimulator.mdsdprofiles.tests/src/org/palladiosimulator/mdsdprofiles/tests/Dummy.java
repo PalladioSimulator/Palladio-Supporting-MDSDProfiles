609a22b64b2395b059497916a1bf7bf0db202d18
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-05-31 21:23:39 +0000

    Moved wrongly placed project
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/Profiles/trunk@26769 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.mdsdprofiles.tests/src/org/palladiosimulator/mdsdprofiles/tests/Dummy.java
@@ -0,0 +1,12 @@
+package org.palladiosimulator.mdsdprofiles.tests;
+
+import org.junit.Test;
+
+public class Dummy {
+
+    @Test
+    public void test() {
+        // see integration tests under org.palladiosimulator.pcm.profiles.tests
+    }
+
+}
