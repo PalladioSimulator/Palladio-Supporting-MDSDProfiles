685d73fa3e59bc8299678c007898e18e0b097682
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2015-06-02 18:21:08 +0000

    Implemented label update
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/Profiles/trunk@26828 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.mdsdprofiles.edit/src-man/org/palladiosimulator/mdsdprofiles/provider/StereotypableElementDecoratorAdapterFactory.java
+++ org.palladiosimulator.mdsdprofiles.edit/src-man/org/palladiosimulator/mdsdprofiles/provider/StereotypableElementDecoratorAdapterFactory.java
@@ -1,8 +1,12 @@
 package org.palladiosimulator.mdsdprofiles.provider;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.edit.provider.DecoratorAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemProviderDecorator;
+import org.eclipse.emf.edit.provider.INotifyChangedListener;
+import org.eclipse.emf.edit.provider.ViewerNotification;
 
 public class StereotypableElementDecoratorAdapterFactory extends DecoratorAdapterFactory {
 
@@ -16,14 +20,18 @@ public class StereotypableElementDecoratorAdapterFactory extends DecoratorAdapte
                 this);
 
         if (((Class<?>) type).isInstance(stereotypableElementItemProviderDecorator)) {
-            // stereotypableElementItemProviderDecorator.addListener(new INotifyChangedListener() {
-            //
-            // @Override
-            // public void notifyChanged(final Notification notification) {
-            // fireNotifyChanged(notification);
-            // }
-            //
-            // });
+            stereotypableElementItemProviderDecorator.addListener(new INotifyChangedListener() {
+
+                @Override
+                public void notifyChanged(final Notification notification) {
+                    fireNotifyChanged(notification);
+                    if (notification.getFeature() == EcorePackage.eINSTANCE.getEModelElement_EAnnotations()) {
+                        // Request a label update of the Annotation's parent object
+                        fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+                    }
+                }
+
+            });
             return stereotypableElementItemProviderDecorator;
         }
         return null;
