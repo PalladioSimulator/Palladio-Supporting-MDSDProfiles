06e6de1b009b41c16bf93f1a9875a0f8af364d0a
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-02 21:12:23 +0000

    Implemented filter for exluding unwanted features (applied to, extension, ...)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/Profiles/trunk@26834 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.mdsdprofiles.edit/src-man/org/palladiosimulator/mdsdprofiles/provider/StereotypableElementItemProviderDecorator.java
+++ org.palladiosimulator.mdsdprofiles.edit/src-man/org/palladiosimulator/mdsdprofiles/provider/StereotypableElementItemProviderDecorator.java
@@ -1,11 +1,13 @@
 package org.palladiosimulator.mdsdprofiles.provider;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -14,6 +16,7 @@ import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemProviderDecorator;
 import org.modelversioning.emfprofile.Stereotype;
+import org.modelversioning.emfprofileapplication.EMFProfileApplicationPackage;
 import org.modelversioning.emfprofileapplication.StereotypeApplication;
 import org.palladiosimulator.mdsdprofiles.StereotypableElement;
 
@@ -26,6 +29,11 @@ import org.palladiosimulator.mdsdprofiles.StereotypableElement;
 public class StereotypableElementItemProviderDecorator extends ItemProviderDecorator implements
         IEditingDomainItemProvider, IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider,
         IItemPropertySource, Adapter {
+
+    protected final static List<Integer> EXLUDED_FEATURE_IDS = Arrays.asList(
+            EMFProfileApplicationPackage.STEREOTYPE_APPLICATION__APPLIED_TO,
+            EMFProfileApplicationPackage.STEREOTYPE_APPLICATION__PROFILE_APPLICATION,
+            EMFProfileApplicationPackage.STEREOTYPE_APPLICATION__EXTENSION);
 
     public StereotypableElementItemProviderDecorator(final AdapterFactory adapterFactory) {
         super(adapterFactory);
@@ -69,6 +77,25 @@ public class StereotypableElementItemProviderDecorator extends ItemProviderDecor
                 || !((StereotypableElement) object).hasStereotypeApplications();
     }
 
+    private void addTaggedValuesPropertyDescriptors(final List<IItemPropertyDescriptor> propertyDescriptors,
+            final StereotypableElement stereotypableElement) {
+        for (final StereotypeApplication stereotypeApplication : stereotypableElement.getStereotypeApplications()) {
+            final IItemPropertySource stereotypeApplicationPropertySource = (IItemPropertySource) this
+                    .getAdapterFactory().adapt(stereotypeApplication, IItemPropertySource.class);
+
+            final List<IItemPropertyDescriptor> stereotypePropertyDescriptors = stereotypeApplicationPropertySource
+                    .getPropertyDescriptors(stereotypeApplication);
+
+            for (final IItemPropertyDescriptor stereotypePropertyDescriptor : stereotypePropertyDescriptors) {
+                final EStructuralFeature feature = (EStructuralFeature) stereotypePropertyDescriptor.getFeature(null);
+                if (!EXLUDED_FEATURE_IDS.contains(feature.getFeatureID())) {
+                    propertyDescriptors.add(new StereotypableElementItemPropertyDescriptorDecorator(
+                            stereotypeApplication, stereotypePropertyDescriptor));
+                }
+            }
+        }
+    }
+
     @Override
     public IItemPropertyDescriptor getPropertyDescriptor(Object object, Object propertyId) {
         if (notStereotyped(object)) {
@@ -82,22 +109,6 @@ public class StereotypableElementItemProviderDecorator extends ItemProviderDecor
         }
 
         throw new RuntimeException("Problem finding property ID -- no clue why :(");
-    }
-
-    private void addTaggedValuesPropertyDescriptors(final List<IItemPropertyDescriptor> propertyDescriptors,
-            final StereotypableElement stereotypableElement) {
-        for (final StereotypeApplication stereotypeApplication : stereotypableElement.getStereotypeApplications()) {
-            final IItemPropertySource stereotypeApplicationPropertySource = (IItemPropertySource) this
-                    .getAdapterFactory().adapt(stereotypeApplication, IItemPropertySource.class);
-
-            final List<IItemPropertyDescriptor> stereotypePropertyDescriptors = stereotypeApplicationPropertySource
-                    .getPropertyDescriptors(stereotypeApplication);
-
-            for (final IItemPropertyDescriptor stereotypePropertyDescriptor : stereotypePropertyDescriptors) {
-                propertyDescriptors.add(new StereotypableElementItemPropertyDescriptorDecorator(stereotypeApplication,
-                        stereotypePropertyDescriptor));
-            }
-        }
     }
 
     @Override
